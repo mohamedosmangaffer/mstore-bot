29782ab2e3c348498267a7a25fdcce48c71af461
Mohamed Osman <mjjjjj2019@gmail.com>
2025-06-17 01:21:53 +0200

    Update and rename requirements.txt to database.py

---
+++ database.py
@@ -0,0 +1,16 @@
+import sqlite3
+import os
+
+DB_FILE = os.path.join(os.path.dirname(__file__), "../../orders.db")
+
+def init_db():
+    with sqlite3.connect(DB_FILE) as conn:
+        conn.execute(\"\"\"CREATE TABLE IF NOT EXISTS orders (
+            user_id INTEGER,
+            name TEXT,
+            service TEXT,
+            time TEXT,
+            attachment TEXT DEFAULT NULL,
+            order_id TEXT DEFAULT NULL,
+            status TEXT DEFAULT 'pending'
+        )\"\"\")
