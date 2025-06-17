93732104b0410466a126b8a16bb4a0050ceb25a0
Mohamed Osman <mjjjjj2019@gmail.com>
2025-06-17 03:05:56 +0200

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ DB_FILE = os.path.join(os.path.dirname(__file__), "../../orders.db")
 
 def init_db():
     with sqlite3.connect(DB_FILE) as conn:
-        conn.execute(\"\"\"CREATE TABLE IF NOT EXISTS orders (
+        conn.execute(\"\"\"CREATE TABLE IF NOT EXISTS orders )
             user_id INTEGER,
             name TEXT,
             service TEXT,
